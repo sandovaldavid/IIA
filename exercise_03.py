95cd1f70bafcce12336d8ca8162dbd6dbd30adb2
sandovaldavid <sandovaldavid2201@gmail.com>
2024-10-25 14:06:35 -0500

    :sparkles: solution to exercise 03

--- exercise_03.py
+++ exercise_03.py
@@ -1,1 +1,29 @@
-#Encuentra la suma de los digitos de un numero entero
+#Encuentra la suma y la cantidad de los digitos de un numero entero
+
+num = int(input("Enter a number: "))
+
+def digitos(num):
+    flag = True
+    digit = 0
+    newnum = num
+    while flag:
+        newnum = int(newnum/10)
+        if newnum != 0:
+            digit = digit + 1
+        else:
+            flag = False
+    return digit+1
+
+print(f"The digits of this number are: {digitos(num)}")
+
+def suma(num):
+    numDig = digitos(num)
+    sum = 0
+    i = 0
+    while i < numDig:
+        sum = sum + (num % 10)
+        num = int(num/10)
+        i = i + 1
+    return sum
+
+print(f"The sum of the digits of this number is: {suma(num)}")
