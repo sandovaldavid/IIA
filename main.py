5ddcf81ee3aff5fb0c1c095c4832fb367bdb886e
sandovaldavid <sandovaldavid2201@gmail.com>
2024-10-25 13:11:55 -0500

    :sparkles: exercise_01.py

---
+++ main.py
@@ -0,0 +1,11 @@
+# Exercise 01: Write a program that asks the user for their data
+name = input("Enter your name: \n")
+age = input("Enter your age: \n")
+sex = input("Enter your sex [Male(M) or Female(f)]: \n")
+height = input("Enter your height: \n")
+
+print("Your data is:", "\n")
+print("Name: ", name, "\n",
+      "Age: ", age, "\n",
+      "Sex: ", sex, "\n",
+      "Height: ", height, "\n")
